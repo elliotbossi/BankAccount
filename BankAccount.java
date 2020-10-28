a979678a674174925cebbd98292b46560e819f08
Elliot <ebossi10@stuy.edu>
2020-10-28 15:58:00 -0400

    withdraw methodd worked

--- BankAccount.java
+++ BankAccount.java
@@ -29,4 +29,14 @@ public class BankAccount {
       return false;
     }
   }
+
+  public boolean withdraw(double amount){
+    if ((amount > balance) || (amount < 0)){
+      return false;
+    }
+    else {
+      balance = balance - amount;
+      return true;
+    }
+  }
 }
